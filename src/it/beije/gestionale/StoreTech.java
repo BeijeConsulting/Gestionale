9aa20c3b5043c75bd8ceac0d2a3a46c6a61b657e
Cilindre1 <giovanni.ciliento@gmail.com>
2019-02-28 15:31:13 +0100

    modifica config

---
+++ src/it/beije/gestionale/StoreTech.java
@@ -0,0 +1,33 @@
+package it.beije.gestionale;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class StoreTech {
+
+	public static void main(String[] args) {
+
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+		
+		for (int i = 0; i < args.length; i++) {
+			Tecnologia tech = new Tecnologia();
+			tech.setTecnologia(args[i]);
+			session.save(tech);
+		}
+		
+		t.commit();
+		System.out.println("Into de tabbella");
+		factory.close();
+		session.close();  
+	}
+}
