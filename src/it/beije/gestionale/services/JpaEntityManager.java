ac37e82082c094b56b5c6184d2dc21b5e433831e
ivfly <ivo.mosca@privategriffe.com>
2019-03-01 17:50:24 +0100

    static factory

--- src/it/beije/gestionale/services/JpaEntityManager.java
+++ src/it/beije/gestionale/services/JpaEntityManager.java
@@ -1,5 +1,25 @@
 package it.beije.gestionale.services;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaEntityManager {
+	
+	private static EntityManagerFactory emfactoryGestionale;
+	//private static EntityManagerFactory emfactoryRapportini;
+	
+	static {
+		emfactoryGestionale = Persistence.createEntityManagerFactory( "Gestionale" );
+		//emfactoryRapportini = Persistence.createEntityManagerFactory( "Rapportini" );
+	}
+	
+	public static EntityManager getGestionaleManager() {
+		return emfactoryGestionale.createEntityManager();
+	}
+
+//	public static EntityManager getRapportiniManager() {
+//		return emfactoryRapportini.createEntityManager();
+//	}
 
 }
